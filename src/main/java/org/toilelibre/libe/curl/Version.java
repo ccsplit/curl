6a6f0797057f41e4ce578788f35e3d01a17d890d
ccsplit <cshay237@gmail.com>
2017-08-25 10:57:49 -0500

    Add minimizeJar true and include commons-logging dependencies.
    Include all of commons-logging since the minimizeJar would
    normally remove these needed classes. This helped to get the jar down to
    ~993K.

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,5 +1,5 @@
 package org.toilelibre.libe.curl;
 public final class Version {
  public static String VERSION = "0.0.13-SNAPSHOT";
- public static String BUILD_TIME = "2017-08-25T15:45:03Z";
+ public static String BUILD_TIME = "2017-08-25T16:01:49Z";
 }
